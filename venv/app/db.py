85968a0bd42bdb95681286b4207a3011870c69b7
Ittay Alfassi <ittay.alfassi@gmail.com>
2020-04-13 12:09:25 +0300

    fixed db from mysql to sqlite
    also started working on the new queries.

--- venv/app/db.py
+++ venv/app/db.py
@@ -1,15 +1,13 @@
-import MySQLdb
-import ConfigParser
+import sqlite3
+import configparser
 
 
 class DBConnection:
 
     def __init__(self):
         config = ConfigParser.ConfigParser()
-        config.read("config.cfg")
-        user = config.get("Data", "user")
-        password = config.get("Data", "password")
-        schema = config.get("Data", "schema")
-        self.db = MySQLdb.connect("localhost", user, password, schema)
+        config.read("dbs/config.cfg")
+        db_name = config.get("Data", "filename")
+        self.db = sqlite3.connect(db_name)
         self.db.set_character_set('utf8')
         self.cursor = self.db.cursor()
